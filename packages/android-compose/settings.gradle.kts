6378d82f1cec6eb3644f62955792ce721e1d1abd
theamodhshetty <theamodhshetty@gmail.com>
2026-02-23 20:39:06 +0530

    fix(android): configure plugin and dependency repositories

--- packages/android-compose/settings.gradle.kts
+++ packages/android-compose/settings.gradle.kts
@@ -1,1 +1,17 @@
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
 rootProject.name = "chartcn-mobile-android-compose"
